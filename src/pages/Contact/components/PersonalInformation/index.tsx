6df025fbb3ec9afb842fb1293ace64eb76bacc93
iczky <fiqra22@gmail.com>
2024-04-28 10:12:17 +0700

    feat: add personal information in Contact Page

--- src/pages/Contact/components/PersonalInformation/index.tsx
+++ src/pages/Contact/components/PersonalInformation/index.tsx
@@ -14,13 +14,31 @@ const ContactDetail = styled.div`
   gap: 8px;
 `;
 
+const PersonalSocMed = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
 const PersonalInformation = () => {
   return (
     <PersonalContainer>
       <img src={Image} alt="Foto contact" />
       <ContactDetail>
         <p>Contact Details</p>
+        <h3>ayush.barnwal@brightscout.com</h3>
+        <h3>+91 8651447521</h3>
       </ContactDetail>
+      <PersonalSocMed>
+        <p>Social</p>
+        <ul>
+          <li>Linkedin</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+          <li>Webflow</li>
+          <li>Figma</li>
+        </ul>
+      </PersonalSocMed>
     </PersonalContainer>
   );
 };
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,23 @@
 import Navbar from "../../components/Navbar";
+import { PersonalInformation } from "./components";
+import styled from "styled-components";
+
+const ContactContainer = styled.section`
+  display: flex;
+  gap: 275px;
+  justify-content: space-between;
+  padding: 173px 80px 100px 80px;
+`;
 
 const Contact = () => {
-  return <Navbar />;
+  return (
+    <>
+      <ContactContainer>
+        <Navbar />
+        <PersonalInformation />
+      </ContactContainer>
+    </>
+  );
 };
 
 export default Contact;
